888c1773ef98292b7a1439c84e12715463996164
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2013-02-28 17:58:32 +0100

    update config data with new app modal data

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
  * Setup your app-model access credentials here
  */
 $aa_activated = true;
-$aa_app_id = 153;
-$aa_app_secret = "770a51440ac4e9c2dbe7345a791fbfc4";
+$aa_app_id = 254;
+$aa_app_secret = "6067c97f08a230a322222507342d4c65";
 
 /**
  * Setup your database access data
  */
-$db_activated = true;
+$db_activated = false;
 $db_host = "localhost";
 $db_name = "apps_aa_template";
 $db_user = "aa_template";
